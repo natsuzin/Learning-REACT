994528bc1e60fe574ef4ddee57dbfaa740fa4915
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2024-10-15 16:31:22 +0000

    First Steps INTRO

---
+++ 1_INTRO/hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World!</h1>
+			<div className="Bio">
+				<p>I'm Nathalia, 22 years old and I study Computer Science at Univali.
+				<br></br>
+				I'm very excited to learn more about React.</p>
+			</div>
+			<img width="351" height="268" src="https://t3.ftcdn.net/jpg/02/18/45/30/360_F_218453068_WRjhtH9MzO33Cb19K8acZI2VZvXueQ70.jpg"
+            alt="Gatito"/>
+		</div>
+	);
+}
+
+export default App;
