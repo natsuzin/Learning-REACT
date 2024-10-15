a8cf0bde32a381779e29147236f9d269ebeabf36
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2024-10-15 16:24:57 +0000

    First steps

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,17 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+
+// styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
